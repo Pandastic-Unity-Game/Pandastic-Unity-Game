f7957eb5d641a27e3308e2c79f838fdac6479777
MrRytis <rytis.janceris@gmail.com>
2018-03-23 13:35:51 +0200

    VEH1 Added speedometer
    Added speedometer and change controls a bit

---
+++ Game project/Assets/Speedometer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Speedometer : MonoBehaviour {
+
+    public Text speedometer;
+    private Rigidbody rb;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetText();
+	}
+    void GetText()
+    {
+        rb = GetComponent<Rigidbody>();
+        float speed = rb.velocity.magnitude*3.6f;
+        string Speed = Math.Round(speed) + " KM/H";
+        speedometer.text = Speed;
+    }
+}
