75d2f0d6ab81a52780ec56ea6f6a6d72b0bdc2d5
Andrius Kondratjev <andriusk1997@gmail.com>
2018-03-26 21:12:27 +0300

    fixing functionality

--- Game project/Assets/LapComplete.cs
+++ Game project/Assets/LapComplete.cs
@@ -53,7 +53,7 @@ public class LapComplete : MonoBehaviour {
 		MilliDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MilliCount;
 
 
-	if (LapTimeManager.MinuteCount< BestMin) {
+       if (LapTimeManager.MinuteCount< BestMin) {
 			
 			ChangeData ();
 		}
@@ -62,14 +62,13 @@ public class LapComplete : MonoBehaviour {
             { 
 				ChangeData();
 			}
-		}
-		else if(LapTimeManager.SecondCount== BestSec)
-		{
-			if (LapTimeManager.MilliCount < BestMilli)
-			{
-				ChangeData();
-			}
-		}
+            else if (LapTimeManager.MilliCount < BestMilli)
+            {
+                ChangeData();
+            }
+        }
+        
+
 
         LapTimeManager.MinuteCount = 0;
         LapTimeManager.SecondCount = 0;
