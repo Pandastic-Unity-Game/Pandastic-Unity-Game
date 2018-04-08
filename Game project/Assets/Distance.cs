7cb334ea8d5316474779263dfe58782d877ded21
Andrius Kondratjev <andriusk1997@gmail.com>
2018-04-08 12:28:41 +0300

    DRP2-Adding script

---
+++ Game project/Assets/Distance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject Car;
+	public float Distance_;
+	public GameObject[] CheckPoint;
+	public int TriggerCounter;
+
+	void Start () {
+		CheckPoint = GameObject.FindGameObjectsWithTag("CheckPointLap");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Distance_ = Vector3.Distance (Car.transform.position, CheckPoint [0].transform.position);
+		Debug.Log (CheckPoint.Length);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag("CheckPointLap")) 
+			{
+			foreach(var obj in CheckPoint)
+			{
+				obj.SetActive (false);
+
+			}
+		
+	}
+}
+
