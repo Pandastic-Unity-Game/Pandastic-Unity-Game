1dccdc4bfa4486cd141477eae32c394388e59773
Mindaugas Rubuževičius <37072263+minde97@users.noreply.github.com>
2018-04-23 20:50:45 +0300

    DRP2
    Fixed some bugs

---
+++ Game project/Assets/LapCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapCount : MonoBehaviour {
+    public Text CurrentLap;
+    public CarPosition lap;
+    // Use this for initialization
+    void Start () {
+        CurrentLap = GetComponent<Text>();
+        CurrentLap.text = "1";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (lap.currentLap!=0)
+        {
+            CurrentLap.text = (lap.currentLap+1).ToString();
+        }
+        
+	}
+}
