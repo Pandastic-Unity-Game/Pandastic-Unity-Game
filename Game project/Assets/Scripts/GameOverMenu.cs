89a8a5895def4a9ff515fa0603c803e6940ca2ae
Aurimas <valauskas.aurimas@gmail.com>
2018-05-06 00:02:33 +0300

    MEN7 - player can choose laps number
    MEN7- player have ability to choose how many laps he want to race

--- Game project/Assets/Scripts/GameOverMenu.cs
+++ Game project/Assets/Scripts/GameOverMenu.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class GameOverMenu : MonoBehaviour
 {
 
     public static bool GameIsOver = false;
     public CarPosition lap;
-    public int Laps;
+    public int Laps1;
     public GameObject GameOverMenuUI;
-    // Use this for initialization
+    private RaceDontDestroy TotalLaps;
+    private GameObject Total;
+
     void Start()
     {
-
+        Total = GameObject.FindGameObjectWithTag("Datas");
+        TotalLaps = Total.GetComponent<RaceDontDestroy>();
+        Laps1 = TotalLaps.data;
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (lap.currentLap == Laps)
+        
+        if (lap.currentLap == Laps1 )
         {
             if (GameIsOver)
             {
